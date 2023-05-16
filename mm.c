ed44bd11219b4d80f190a8bcda3ee652f6fc49a3
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-16 15:55:14 +0900

    feat : extend_heap
    - 새 가용 블록으로 힙 확장하기

--- mm.c
+++ mm.c
@@ -95,6 +95,28 @@ int mm_init(void)
     return 0;
 }
 
+/* extend_heap */
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    /* Allocate an even number of words to maintain alignment */
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+    {
+        return NULL;
+    }
+
+    /* Initialize free blocks header/footer and the epilogue header */
+    PUT(HDRP(bp), PACK(size, 0));   /* Free block header */
+    PUT(FTRP(bp), PACK(size, 0));   /* Free block footer */
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));   /* New epilogue header */
+    
+    /* Coalesce if the previous block was free */
+    return coalesce(bp);
+}
+
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
