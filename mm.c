f4bfe997337059af7d52c6e50b448c06814c3913
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-16 15:42:58 +0900

    feat : mm_init
    - 최초 가용 블록으로 힙 생성하기

--- mm.c
+++ mm.c
@@ -76,6 +76,22 @@ static void *heap_listp;
  */
 int mm_init(void)
 {
+   /* Create the initial empty heap */
+   if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)  
+   { 
+    return -1;
+   }
+   PUT(heap_listp, 0);                           /* Alignment padding */
+   PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));  /* Prologue header */
+   PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));  /* Prologue footer */
+   PUT(heap_listp + (3*WSIZE), PACK(0, 1));      /* Epilogue header */
+   heap_listp += (2*WSIZE);     
+
+   /* Extend the empty heap with a free block of CHUNKSIZE bytes*/
+   if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+   {
+    return -1;
+   }
     return 0;
 }
 
